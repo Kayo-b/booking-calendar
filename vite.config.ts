d7496ae46272380e284f9d0d168c172f1f1e2f93
kayo-b <kyo.bhz@gmail.com>
2024-04-15 03:20:32 +0200

    More config changes to try to make vue deploy in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import svgLoader from 'vite-svg-loader'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,9 +13,15 @@ export default defineConfig({
     svgLoader()
   ],
   base: '/booking-calendar/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+   build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, './index.html'), // Ensure this is pointing to your root HTML file
     }
   }
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
 })
